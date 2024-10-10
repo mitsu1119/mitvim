c3a943213dc4b5cb2d2acd6b68a8e87e54310a0a
mitsu1119 <hachimitsu1119@gmail.com>
2024-10-10 13:13:33 +0900

    文字入力のループ

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdin, stdout, Error, Read, Write};
+
+fn main() -> Result<(), Error> {
+    loop {
+        print!("> ");
+        stdout().flush()?;
+
+        let mut input = [0];
+        stdin().read(&mut input)?;
+
+        match input[0] {
+            b'q' => break,
+            _ => {
+                println!("{:?}", input);
+            }
+        }
+    }
+
+    Ok(())
 }
